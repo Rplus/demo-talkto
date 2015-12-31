38d77fd4b031ffded804c59d0957ead6a0b48a92
Rplus <rplus.tw@gmail.com>
2015-12-31 17:04:02 +0800

    add tasks for image optimization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,7 +6,9 @@ import plumber from 'gulp-plumber';
 import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
 import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
 import browserSync from 'browser-sync';
+import svgstore from 'gulp-svgstore';
 import jade from 'gulp-jade';
 import sass from 'gulp-sass';
 import postcss from 'gulp-postcss';
@@ -50,6 +52,50 @@ gulp.task('css', () => {
 
 gulp.task('js', () => {
 });
+
+gulp.task('image', () => {
+  gulp.src([
+    appPath.srcDir + 'images/**/*.*',
+    `!${appPath.srcDir}images/svg-icon/*.*`
+    ], {base: appPath.srcDir})
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(gulp.dest(appPath.distDir));
+});
+
+let svgOptions = [
+  { removeTitle: true },
+  { removeUselessStrokeAndFill: true }
+];
+
+gulp.task('svg-icon', () => {
+  gulp.src(`${appPath.srcDir}images/svg-icon/*.*`)
+    .pipe(imagemin({
+      svgoPlugins: svgOptions
+    }))
+    .pipe(rename({prefix: 'icon-'}))
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('svg-icon.svg'))
+    .pipe(gulp.dest(appPath.distDir + 'images'));
+});
+
+gulp.task('team-icon', () => {
+  gulp.src(`${appPath.srcDir}images/team/**.*`)
+    .pipe(imagemin({
+      svgoPlugins: svgOptions
+    }))
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('team-icon.svg'))
+    .pipe(gulp.dest(appPath.distDir + 'images'));
+});
+
+gulp.task('icons', ['svg-icon', 'team-icon']);
+
 
 gulp.task('build', ['hmtl', 'css', 'js', 'image'], () => {
 });
